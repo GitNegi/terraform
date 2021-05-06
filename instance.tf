63fb5352dc7a2fbe03251593f9456f984c8b3dc3
GitNegi <66105456+GitNegi@users.noreply.github.com>
2021-05-06 20:34:45 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,24 @@
+provider "aws"{
+  profile = "testing"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "os1"{
+
+ ami = "ami-0de1d9ec99cd6630c"
+ instance_type = "t2.micro"
+ tags = {
+     Name = "hadoop-os-1"
+
+   }
+}
+
+resource "aws_instance" "os2"{
+
+ ami = "ami-0de1d9ec99cd6630c"
+ instance_type = "t2.micro"
+ tags = {
+     Name = "hadoop-os-2"
+
+   }
+}
